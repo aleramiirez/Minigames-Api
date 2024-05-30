2a82ba605da11836f2aeb9fb0e8a4393123f4ae8
Ale <alejandro1052004@gmail.com>
2024-05-30 22:56:01 +0200

    Eureka service Fixed bugs

--- discovery-server/src/main/java/com/minijuegos/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/minijuegos/discovery_server/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.minijuegos.userfunctions;
+package com.minijuegos.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class UserfunctionsApplication {
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserfunctionsApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
